264ebdcbfcd92ede37b9742c1674addd8af2a0ea
navid bahrami <navidbahrami1996@gmail.com>
2024-12-18 18:27:21 +0330

    first commit

---
+++ auth-service/model/user.js
@@ -0,0 +1,13 @@
+const {default : mongoose} = require('mongoose')
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+} , {timestamps: true})
+
+
+
+const userModel = mongoose.model("user", userSchema);
+module.exports = {
+    userModel
+}
